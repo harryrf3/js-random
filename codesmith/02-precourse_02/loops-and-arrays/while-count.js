1a81338fbb11aa067cdde654895e0f4f3d2fe0ea
Harry Fahringer III <viccem.dev@gmail.com>
2022-02-10 17:24:36 -0500

    A while loop that increments by 2 while count is less than 8.

---
+++ codesmith/02-precourse_02/loops-and-arrays/while-count.js
@@ -0,0 +1,16 @@
+let count = 2;
+// ADD CODE HERE
+
+/*
+ * A while loop that increments by 2
+ * while count is less than 8. 
+ *-
+ * @harryrf3
+**/
+while (count < 8) {
+  count += 2;
+  // console.log(count);
+}
+
+// Uncomment these to check your work!
+console.log(count);
