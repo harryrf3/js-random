8cc16375eb590641a0cc55ed9e5f645656725d28
Harry Fahringer III <viccem.dev@gmail.com>
2022-02-10 06:37:03 -0500

    Moved around files

---
+++ codesmith/02-precourse_02/loops-and-arrays/06-bio.js
@@ -0,0 +1,14 @@
+const firstNames = ["Jon", "Arya", "Jamie"];
+const lastNames = ["Snow", "Stark", "Lannister"];
+const places = ["The Wall", "Winterfell", "Kings Landing"];
+
+
+const bios = [];
+
+// Loop through your arrays and store the following strings in the bios array:
+// 'My name is Jon Snow and I am from The Wall'
+// 'My name is Arya Stark and I am from Winterfell'
+// 'My name is Jamie Lannister and I am from Kings Landing'
+
+// ADD CODE HERE
+
