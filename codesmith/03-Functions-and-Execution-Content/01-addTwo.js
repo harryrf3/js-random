690e33bee9cd3c1b423891b6917d06edb4826f57
Harry Fahringer III <viccem.dev@gmail.com>
2022-02-11 14:31:38 -0500

    Created a simple function that takes in a number and returns the number plus two.

---
+++ codesmith/03-Functions-and-Execution-Content/01-addTwo.js
@@ -0,0 +1,16 @@
+/**
+ * Create a function addTwo that accepts
+ * a number, and returns the number plus 2.
+ *-
+ * @harryrf3 (2022-02-11T14:25:20.000-05:00)
+ ***/
+
+function addTwo(number) {
+  return number + 2;
+}
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+console.log(typeof addTwo); // should log: 'function'
+console.log(addTwo(10)); // should log: 12
