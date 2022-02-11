a94b979cfff739659378d7a4bc02705c4d5671d9
Harry Fahringer III <viccem.dev@gmail.com>
2022-02-11 15:39:58 -0500

    Created a simple function that takes a name as an input and returns `Hi, ${name}`

---
+++ codesmith/03-Functions-and-Execution-Content/03-sayHello.js
@@ -0,0 +1,16 @@
+/**
+ * Create a function sayHello that accepts a 
+ * string argument and returns the string 'Hi',
+ * with the passed-in string added to the end.
+ *-
+ * @harryrf3 (2022-02-11T15:34:26.000-05:00)
+ ***/
+
+// ADD CODE HERE
+function sayHello(name) {
+  return `Hi, ${name}`
+}
+
+// Uncomment these to check your work!
+console.log(sayHello('Mary')); // should log: 'Hi, Mary'
+console.log(sayHello('Haley')); // should log: 'Hi, Haley'
