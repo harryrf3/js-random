02e574e38e95cf5598e228cc206c292846f5e4aa
Harry Fahringer III <viccem.dev@gmail.com>
2022-02-11 19:18:32 -0500

    A simple function that takes two arguments and returns them in a string

---
+++ codesmith/03-Functions-and-Execution-Content/04-wereAwesome.js
@@ -0,0 +1,20 @@
+/**
+ * Write a function wereAwesome that takes you
+ * and a partners name and returns a string 
+ * '[your partner's name] and [you] are awesome!'
+ * 
+ *-
+ * @harryrf3 (2022-02-11T15:41:13.000-05:00)
+ ***/
+
+ function wereAwesome(you, yourBuddy) {
+  // ADD CODE HERE
+  return `${yourBuddy} and ${you} are awesome!`
+}
+
+
+// Uncomment the lines below to test your function:
+console.log(wereAwesome("Dave", "Will")); // => "Will and Dave are awesome!"
+console.log(wereAwesome("Victoria", "Jenny")); // => "Jenny and Victoria are awesome!"
+console.log(wereAwesome("Chris", "Jac")); // => "Jac and Chris are awesome!"
+console.log(wereAwesome("Phillip", "Skyler")); // => "Skyler and Phillip are awesome!"
