866cb51ab2b237c364253662d263443bc3c0f741
Harry Fahringer III <viccem.dev@gmail.com>
2022-02-18 02:47:05 -0500

    Revised code to be cleaner and more functional using the Object.keys() method.

---
+++ codesmith/02-fundamentals/homework/hw_13-iterating-objects-for_in.js
@@ -0,0 +1,58 @@
+/**
+ * You are provided with an empty array objToArray
+ * Using a for...in loop, fill the array with
+ * all of the numbers from checkObj if
+ * they are greater than or equal to 2.
+ *
+ * * More info at bottom of file *
+ *-
+ * @harryrf3 (2022-02-15T08:16:57.000-05:00)
+ ***/
+
+const checkObj = {
+  oddNum: 1,
+  evenNum: 2,
+  foundNum: 5,
+  randomNum: 18,
+};
+
+const objToArray = [];
+
+const returnedKeys = Object.keys(checkObj);
+
+for (const property in returnedKeys) {
+  if (property >= 2) {
+    objToArray.push(property);
+  }
+}
+
+console.log(objToArray);
+
+/**
+ * Revisited this code after having more time to
+ * research on Object's methods and utilizing
+ * a for...in loop to enumerate its properties.
+ *
+ *-
+ * @harryrf3 (2022-02-18T02:37:28.000-05:00)
+ *
+ ***/
+
+
+/* My code before better research; for the curious types */
+// const checkObj = {
+//   oddNum: 1,
+//   evenNum: 2,
+//   foundNum: 5,
+//   randomNum: 18,
+// };
+
+// const objToArray = [];
+// // ADD CODE HERE
+// for (const property in checkObj) {
+//   if (checkObj[property] >= 2) {
+//     objToArray.push(checkObj[property]);
+//   }
+// }
+
+// console.log(objToArray);
