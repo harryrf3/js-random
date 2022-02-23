5ddcc4b974316f0fbb313881dd083318a79f07b3
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-02-23 17:15:17 -0500

    A simple console.log that shows the Function object for add20

---
+++ codesmith/03-Functions-and-Execution-Content/09-display-vs-execution.js
@@ -0,0 +1,14 @@
+/**
+ * Create a console log that displays the contents of the function rather than executing it.
+ * For example, your result should be function add20 (num){ return num + 20 }.
+ *
+ *-
+ * @harryrf3 (2022-02-23T17:12:27.000-05:00)
+ ***/
+
+function add20(num) {
+  return num + 20;
+}
+
+// CREATE YOUR CONSOLE.LOG BELOW
+console.log(add20);
