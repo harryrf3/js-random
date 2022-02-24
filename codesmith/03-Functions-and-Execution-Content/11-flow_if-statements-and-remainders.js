c2e293d6250e136b3d2a4edd6c2040764365a1e0
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-02-23 21:08:48 -0500

    Created a function that checks if 'num' is even or odd

--- codesmith/03-Functions-and-Execution-Content/11-flow_if-statements-and-remainders.js
+++ codesmith/03-Functions-and-Execution-Content/11-flow_if-statements-and-remainders.js
@@ -1,1 +1,22 @@
+/**
+ * Write a function iLoveEvenNumbers that takes a number argument and
+ * returns 'Oh Yeah Evens!' if the argument is even
+ * or 'I am too normal for odd numbers' if the argument is odd.
+ *-
+ * @harryrf3 (2022-02-23T21:03:15.000-05:00)
+ ***/
 
+// ADD CODE HERE
+function iLoveEvenNumbers(num) {
+  if (num % 2 === 0) {
+    // return an even number
+    console.log('EVEN:', num);
+    return 'Oh Yeah Evens!';
+  }
+  console.log('ODD:', num);
+  return 'I am too normal for odd numbers';
+}
+
+// Uncomment these to check your work!
+console.log(iLoveEvenNumbers(42)); // expected log 'Oh Yeah Evens!'
+console.log(iLoveEvenNumbers(17)); // expected log 'I am too normal for odd numbers'
