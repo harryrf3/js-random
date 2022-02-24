a3fe7cb9e8c2610cc2487a77ceacc866aadf0b9e
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-02-23 21:28:01 -0500

    A function that checks the time by input and returns a greeting based on time.

---
+++ codesmith/03-Functions-and-Execution-Content/13-flow_if-statements-and-multiple-conditions.js
@@ -0,0 +1,25 @@
+/**
+ * Using an IF/ELSE statement, write a function greetings
+ * which returns 'Good Morning!' if the hour is before 12,
+ * 'Good Afternoon!' if the hour is before 15,
+ * or 'Good Night!' if the hour is 15 or after.
+ *-
+ * @harryrf3 (2022-02-23T21:21:43.000-05:00)
+ ***/
+
+ function greetings(hour) {
+  // ADD CODE HERE
+  if (hour < 12) {
+    return 'Good Morning!';
+  } else if (hour >= 12 && hour < 15) {
+    return 'Good Afternoon!';
+  }
+  return 'Good Night!';
+}
+
+// Uncomment these to check your work!
+console.log(greetings(8)); // expected log 'Good Morning!'
+console.log(greetings(12)); // expected log 'Good Afternoon!'
+console.log(greetings(14)); // expected log 'Good Afternoon!'
+console.log(greetings(15)); // expected log 'Good Night!'
+console.log(greetings(18)); // expected log 'Good Night!'
