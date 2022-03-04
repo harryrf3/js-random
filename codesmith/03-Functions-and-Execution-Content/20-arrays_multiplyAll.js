90d08337d711038debdcbc1b434c58a49c75a8c9
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-04 09:41:24 -0500

    Created file for multiplyAll.js

---
+++ codesmith/03-Functions-and-Execution-Content/20-arrays_multiplyAll.js
@@ -0,0 +1,13 @@
+/**
+ * Create a function multiplyAll that takes an unknown number of integer arguments,
+ * multiplies them all together,
+ * and returns the result.
+ *-
+ * @harryrf3 (2022-03-04T09:40:16.000-05:00)
+ ***/
+
+// ADD CODE HERE 
+
+// Uncomment these to check your work!
+// console.log(multiplyAll(9, 4, 5, 6, 7, 2, 1, 8, 3)) // should log: 362880
+// console.log(multiplyAll(5, 5, 5, 3)) // should log: 375
