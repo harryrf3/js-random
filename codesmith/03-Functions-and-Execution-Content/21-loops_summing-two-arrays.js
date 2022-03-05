c96feec8a8a3816557d04d9ef23c01fb578dfd5f
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-05 14:01:31 -0500

    Created file for challenge

---
+++ codesmith/03-Functions-and-Execution-Content/21-loops_summing-two-arrays.js
@@ -0,0 +1,15 @@
+/**
+ * Write a function mergingElements which adds each element in array1 to the corresponding element of array2
+ * and returns the new array.
+ *-
+ * @harryrf3 (2022-03-04T23:12:15.000-05:00)
+ ***/
+
+function mergingElements(array1, array2){
+  // ADD CODE HERE
+  
+}
+
+// Uncomment these to check your work!
+// console.log(mergingElements([1, 2, 3, 4], [5, 6, 7, 8])); // expected log [6, 8, 10, 12]
+// console.log(mergingElements([7, 3, 6, 0], [3, 9, 17, 81])); // expected log [10, 12, 23, 81]
