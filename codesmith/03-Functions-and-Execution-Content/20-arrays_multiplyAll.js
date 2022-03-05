befed61d118e7506af76a43c388fe43f1cb6ec82
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-04 23:02:07 -0500

    A function that allows an unspecified amount of arguments to be multiplied.

--- codesmith/03-Functions-and-Execution-Content/20-arrays_multiplyAll.js
+++ codesmith/03-Functions-and-Execution-Content/20-arrays_multiplyAll.js
@@ -6,8 +6,32 @@
  * @harryrf3 (2022-03-04T09:40:16.000-05:00)
  ***/
 
+// The Rest Operator '...' allows for an unlimited amount of arguments
+function multiplyAll(...args) {
+  const result = args.reduce((total, item) => {
+    console.log('total:', total);
+    console.log('item:', item);
+    return item ? item * total : item + total;
+  }, 1);
+  return result;
+}
+
+
+/*
+Alternate way of achieving the same result.
+
+function multiplyAll() {
+  const args = [...arguments];
+  const result = args.reduce(total, item) => {
+    retrun item ? item * total : item + total;
+  }, 1);
+  return result;
+}
+ */
+
 // ADD CODE HERE 
 
+
 // Uncomment these to check your work!
-// console.log(multiplyAll(9, 4, 5, 6, 7, 2, 1, 8, 3)) // should log: 362880
-// console.log(multiplyAll(5, 5, 5, 3)) // should log: 375
+console.log(multiplyAll(9, 4, 5, 6, 7, 2, 1, 8, 3)); // should log: 362880
+console.log(multiplyAll(5, 5, 5, 3)); // should log: 375
