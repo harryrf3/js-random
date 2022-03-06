f166a2fd53510291a51a6e0349e6a8a361b7725b
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-05 23:28:25 -0500

    Created .js file

---
+++ codesmith/03-Functions-and-Execution-Content/22-loops-and-conditionals_loops-and-flow-control.js
@@ -0,0 +1,15 @@
+/**
+ * Write a function mergingTripletsAndQuints which takes in two arrays as arguments.
+ * This function will return a new array replacing the elements in array1 if they are divisible by 3 or 5.
+ * The number should be replaced with the sum of itself added to the element at the corresponding index in array2.
+ *-
+ * @harryrf3 (2022-03-05T23:24:07.000-05:00)
+ ***/
+
+ function mergingTripletsAndQuints(array1, array2) {
+  // ADD CODE HERE
+ }
+ 
+ // Uncomment these to check your work!
+ // console.log(mergingTripletsAndQuints([1, 2, 3, 4, 5, 15], [1, 3, 6, 7, 8, 9])); // expected log [1, 2, 9, 4, 13, 24]
+ // console.log(mergingTripletsAndQuints([1, 1, 3, 9, 5, 15], [1, 2, 3, 4, 5, 6])); // expected log [1, 1, 6, 13, 10, 21]
