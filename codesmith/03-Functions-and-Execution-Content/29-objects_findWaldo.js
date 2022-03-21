d601a26ab37125cea9c66b975b555e34cc230db3
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-20 20:59:40 -0400

    Challenge base template

---
+++ codesmith/03-Functions-and-Execution-Content/29-objects_findWaldo.js
@@ -0,0 +1,14 @@
+/**
+ * Create a function findWaldo that accepts an object and returns the value associated with the key 'Waldo'.
+ * If the key 'Waldo' is not found, the function should return 'Where's Waldo?'
+ * -
+ * @harryrf3 (2022-03-20T20:58:35.000-05:00)
+ ***/
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const DC = {'Bruce': 'Wayne', 'Harley': 'Quinn'}
+// const supernatural = {'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown'}
+// console.log(findWaldo(DC)) // should log: 'Where's Waldo?'
+// console.log(findWaldo(supernatural)) // should log: 'unknown'
