dad9ab21ed6b7501d9dac8705158883e7578e846
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-22 15:32:09 -0400

    Not happy with 'wording' of code, I plan to revisit.

--- codesmith/03-Functions-and-Execution-Content/29-objects_findWaldo.js
+++ codesmith/03-Functions-and-Execution-Content/29-objects_findWaldo.js
@@ -6,9 +6,20 @@
  ***/
 
 // ADD CODE HERE
-
+const findWaldo = function (obj) {
+  let result;
+  for (const value in obj) {
+    if (value == 'Waldo') {
+      result = obj[value]; // I don't like how this I 'worded' this, can't seem to make the code 'read' in a way that it makes more sense.
+    } else {
+      result = 'Where\'s Waldo?';
+    }
+  }
+  return result;
+};
 // Uncomment these to check your work!
-// const DC = {'Bruce': 'Wayne', 'Harley': 'Quinn'}
-// const supernatural = {'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown'}
-// console.log(findWaldo(DC)) // should log: 'Where's Waldo?'
-// console.log(findWaldo(supernatural)) // should log: 'unknown'
+const DC = { 'Bruce': 'Wayne', 'Harley': 'Quinn' };
+const supernatural = { 'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown' };
+
+console.log(findWaldo(DC)); // should log: 'Where's Waldo?'
+console.log(findWaldo(supernatural)); // should log: 'unknown'
