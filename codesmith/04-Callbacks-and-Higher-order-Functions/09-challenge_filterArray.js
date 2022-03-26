6ba5074a7f64b1c59454d92bcabad3e95d14eb63
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-26 06:11:45 -0400

    func1 and func2 to check for even and odd nums

--- codesmith/04-Callbacks-and-Higher-order-Functions/09-challenge_filterArray.js
+++ codesmith/04-Callbacks-and-Higher-order-Functions/09-challenge_filterArray.js
@@ -4,7 +4,7 @@
  * @harryrf3 (2022-03-26T06:03:51.000-05:00)
  ***/
 
- function filterArray(array, callback) {
+function filterArray (array, callback) {
   const newArray = [];
   for (let i = 0; i < array.length; i += 1) {
     if (callback(array[i])) newArray.push(array[i]);
@@ -13,14 +13,16 @@
 }
 
 const arrOfNums = [1, 2, 3, 4, 5];
-function func1(num) {
+function func1 (num) {
   // ADD CODE HERE
+  return num % 2 === 0;
 }
 
-function func2(num) {
+function func2 (num) {
   // ADD CODE HERE
+  return num % 2;
 }
 
 // Uncomment these to check your work!
-// console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
-// console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
