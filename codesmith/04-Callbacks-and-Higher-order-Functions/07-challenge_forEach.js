f84da187fad856cb58853bd19cb24d0fc07fb2f0
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-25 23:31:39 -0400

    The abstraction was a bit confusing. I have it down now.

--- codesmith/04-Callbacks-and-Higher-order-Functions/07-challenge_forEach.js
+++ codesmith/04-Callbacks-and-Higher-order-Functions/07-challenge_forEach.js
@@ -21,8 +21,29 @@
 
 // ADD CODE HERE
 
+function forEach (array, callback) {
+  // iterate through our array with a for loop
+  for (let i = 0; i < array.length; i++) {
+    // invoke the callback function, passing in the current element of the array 
+    callback(array[i]);
+  }
+}
+
+function map (arr, call) {
+  // initialize a new array
+  const output = [];
+  //  call forEach with the passed in array and a new function that will take an array element
+  forEach(arr, function (element) {
+    // invoke the passed in callback function with the current array element given to us by forEach
+    output.push(call(element));
+    // add the return value to our array
+  });
+  // return our array containing the results
+  return output;
+}
+
 // Uncomment these to check your work!
-// console.log(typeof forEach); // should log: 'function'
-// forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
-// console.log(typeof map); // should log: 'function'
-// console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
+console.log(typeof forEach); // should log: 'function'
+forEach(['a', 'b', 'c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+console.log(typeof map); // should log: 'function'
+console.log(map([3, 4, 5], n => n - 2)); // should log: [1, 2, 3]
