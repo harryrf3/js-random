cc7166364b98f566c895490106d7e4a672373251
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-26 06:06:57 -0400

    Base challenge code

---
+++ codesmith/04-Callbacks-and-Higher-order-Functions/09-challenge_filterArray.js
@@ -0,0 +1,26 @@
+/**
+ * Add code to the functions func1 and func2 in the places marked "ADD CODE HERE" in order to achieve the desired console logs.
+ * -
+ * @harryrf3 (2022-03-26T06:03:51.000-05:00)
+ ***/
+
+ function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+const arrOfNums = [1, 2, 3, 4, 5];
+function func1(num) {
+  // ADD CODE HERE
+}
+
+function func2(num) {
+  // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+// console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+// console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
