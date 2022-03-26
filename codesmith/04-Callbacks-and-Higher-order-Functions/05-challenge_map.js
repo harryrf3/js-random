acd75b15067c2f30ee74f919452d2c4ae0609a9b
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-25 21:47:40 -0400

    Base challenge code

---
+++ codesmith/04-Callbacks-and-Higher-order-Functions/05-challenge_map.js
@@ -0,0 +1,24 @@
+/**
+ * Create a function subtractTwo that accepts a number and returns that number minus 2.
+ * 
+ * Then create a function map that takes two inputs -
+ * 1. an array of numbers (a list of numbers)
+ * 2. a 'callback' function - this function is applied to each element of the array (inside of the function 'map')
+ * 
+ * Have your map function return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array.
+ * 
+ * Please do not use the native map or forEach method.
+ * 
+ * map([3,4,5], subtractTwo); //-> [1,2,3]
+ * subtractTwo(10); //-> 8
+ * subtractTwo(12); //-> 10
+ * -
+ * @harryrf3 (2022-03-25T21:45:23.000-05:00)
+ ***/
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// console.log(typeof subtractTwo); // should log: 'function'
+// console.log(typeof map); // should log: 'function'
+// console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
