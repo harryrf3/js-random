8d9ff7da58769972d4be507a2538af6628cf02eb
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-25 22:15:03 -0400

    Base challenge code

---
+++ codesmith/04-Callbacks-and-Higher-order-Functions/07-challenge_forEach.js
@@ -0,0 +1,28 @@
+/**
+ * Part 1
+ * 
+ * Create a function forEach which takes an array and a callback, and runs the callback on each element of the array.
+ * forEach does not return anything. Please do not use the native forEach or map method.
+ * 
+ * let alphabet = '';
+ * const letters = ['a', 'b', 'c', 'd'];
+ * forEach(letters, function(char) {
+ *   alphabet += char;
+ * });
+ * console.log(alphabet); //prints 'abcd'
+ * 
+ * Part 2
+ * 
+ * Now let's rebuild map from the previous challenge.
+ * This time instead of using a for loop, you're going to use the forEach we just created.
+ * -
+ * @harryrf3 (2022-03-25T22:11:11.000-05:00)
+ ***/
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// console.log(typeof forEach); // should log: 'function'
+// forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+// console.log(typeof map); // should log: 'function'
+// console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
