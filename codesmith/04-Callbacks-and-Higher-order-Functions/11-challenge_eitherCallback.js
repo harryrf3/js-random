463aaf0efe93867d8bcf5c46d84520a7cfacaa51
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-26 15:03:37 -0400

    A function that returns either callback based on which is true

--- codesmith/04-Callbacks-and-Higher-order-Functions/11-challenge_eitherCallback.js
+++ codesmith/04-Callbacks-and-Higher-order-Functions/11-challenge_eitherCallback.js
@@ -8,20 +8,25 @@
  * @harryrf3 (2022-03-26T06:38:55.000-05:00)
  ***/
 
- function eitherCallback(callback1, callback2) {
+function eitherCallback (callback1, callback2) {
   // ADD CODE HERE
+  // return a function containing parameters of conditional in filterArray for loop
+  return (element, index, array) => { // index and array are not needed, but I kept them here for referring back to as a 'note' later
+    // return boolean if callback is true
+    return callback1(element) || callback2(element);
+  };
 }
 
 // Uncomment these to check your work!
-// function filterArray(array, callback) {
-//   const newArray = [];
-//   for (let i = 0; i < array.length; i += 1) {
-//     if (callback(array[i], i, array)) newArray.push(array[i]);
-//   }
-//   return newArray;
-// }
-// const arrOfNums = [10, 35, 105, 9];
-// const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
-// const over100 = n => n > 100;
-// const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
-// console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
+function filterArray (array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i], i, array)) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const arrOfNums = [10, 35, 105, 9];
+const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
+const over100 = n => n > 100;
+const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
+console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
