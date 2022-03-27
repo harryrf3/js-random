3c0e16baa209a47f12b950acb6ef6e95a48a1665
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-27 16:03:38 -0400

    Base challenge code and instructions

---
+++ codesmith/04-Callbacks-and-Higher-order-Functions/15-challenge_intersection.js
@@ -0,0 +1,14 @@
+/**
+ * Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs.
+ * BONUS - Use reduce!
+ * -
+ * @harryrf3 (2022-03-27T16:03:02.000-05:00)
+ ***/
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const arr1 = [5, 10, 15, 20];
+// const arr2 = [15, 88, 1, 5, 7];
+// const arr3 = [1, 10, 15, 5, 20];
+// console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
