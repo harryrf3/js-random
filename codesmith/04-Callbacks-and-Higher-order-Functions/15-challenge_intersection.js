02eb5c0d848a8704e6703e309091b04a93b90133
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-03-30 20:07:29 -0400

    A function that checks for and returns intersecting values

--- codesmith/04-Callbacks-and-Higher-order-Functions/15-challenge_intersection.js
+++ codesmith/04-Callbacks-and-Higher-order-Functions/15-challenge_intersection.js
@@ -6,9 +6,30 @@
  ***/
 
 // ADD CODE HERE
+function intersection (input) {
+  // accumulator starts as one of the arrays
+  let result = input.pop();
+  // loop through array of arrays
+  while (input.length) {
+    const current = input.pop();
+    const newArr = [];
+    // for each element in current
+    current.filter((element) => {
+      // if that element exists in accumulator
+      if (result.includes(element))
+        // push that element into newArray
+        newArr.push(element);
+    });
+    // accumulator reassigned to newArray
+    result = newArr;
+  }
+  console.log(result);
+  // return accumulator
+  return result;
+}
 
 // Uncomment these to check your work!
-// const arr1 = [5, 10, 15, 20];
-// const arr2 = [15, 88, 1, 5, 7];
-// const arr3 = [1, 10, 15, 5, 20];
-// console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
+const arr1 = [5, 10, 15, 20];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [1, 10, 15, 5, 20];
+console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
