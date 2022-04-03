3a85dd67509b49e8f1d6071ccb28cef5d676652f
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-04-03 14:04:05 -0400

    Reduced input down to a flat array, in order, without repeating elements

--- codesmith/04-Callbacks-and-Higher-order-Functions/17-challenge_union.js
+++ codesmith/04-Callbacks-and-Higher-order-Functions/17-challenge_union.js
@@ -10,9 +10,21 @@
  ***/
 
 // ADD CODE HERE
+function union (input) {
+  // use reduce to iterate through outer array of arrays (return eval'd result)
+  return input.reduce((outputArr, currentArr) => {
+    // iterate thru each inner arr to access each element
+    currentArr.forEach(el => {
+      // if element is not present in output
+      if (!outputArr.includes(el)) outputArr.push(el)// add element to output
+    });
+    // return output
+    return outputArr;
+  }, []); // init empty arr
+}
 
 // Uncomment these to check your work!
-// const arr1 = [5, 10, 15];
-// const arr2 = [15, 88, 1, 5, 7];
-// const arr3 = [100, 15, 10, 1, 5];
-// console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
+const arr1 = [5, 10, 15];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [100, 15, 10, 1, 5];
+console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
