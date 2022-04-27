5e0c6702709348813014a34a7adcb2257fa15016
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-04-27 15:54:45 -0400

    updated comments

--- codesmith/04-Callbacks-and-Higher-order-Functions/19-challenge_objOfMatches.js
+++ codesmith/04-Callbacks-and-Higher-order-Functions/19-challenge_objOfMatches.js
@@ -1,14 +1,36 @@
 /**
  * Construct a function objOfMatches that accepts two arrays and a callback.
+ *
  * objOfMatches will build an object and return it.
+ * 
  * To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array.
+ * 
  * If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value.
  * -
  * @harryrf3 (2022-04-03T17:03:26.000-05:00)
- ***/
+*/
 
 
 // ADD CODE HERE
+
+
+/*
+ * First array: use callback to test each element to see if output matches
+ * corresponding element by index of the second array.
+*/
+
+const objOfMatches = (prev, index, callback) => {
+  /*
+   * If match is found: (use elements to modify object)
+   *  # first array: element = key
+   *  # second array: element = value
+  */
+
+  //
+  
+}
+
+
 
 // Uncomment these to check your work!
 // const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
