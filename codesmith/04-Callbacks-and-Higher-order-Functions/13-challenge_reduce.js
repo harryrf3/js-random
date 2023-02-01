717500be6825a21549a2e424f9332eeb845f3158
Harry Fahringer \|/ <2272430+harryrf3@users.noreply.github.com>
2023-02-01 14:17:48 -0500

    Update 13-challenge_reduce.js

--- codesmith/04-Callbacks-and-Higher-order-Functions/13-challenge_reduce.js
+++ codesmith/04-Callbacks-and-Higher-order-Functions/13-challenge_reduce.js
@@ -1,7 +1,10 @@
 /**
   The function `reduce` takes an array and reduces the elements to a single value.
 
-  The `reduce` function loops through the array and applies any operation that you can put into a function to each element in the array while keeping track of the outcome of each loop. In this way, we could use `reduce` to do things like sum all the numbers in an array or multiply them all together.
+  The `reduce` function loops through the array and applies any operation that you can put into a function to each element in the array while keeping track 
+  of the outcome of each loop.
+  
+  In this way, we could use `reduce` to do things like sum all the numbers in an array or multiply them all together.
 
   const nums = [4, 1, 3];
   const add = function(a, b) { return a + b; }
