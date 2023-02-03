24f9e5beb65d03aafa3a0d8b1c7ecf7159f8d18b
Harry Fahringer III <viccem.dev@gmail.com>
2023-02-03 08:46:34 -0500

    .

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -1,21 +1,28 @@
-import fetch from 'fetch';
-// Grab data from fetch input for daily challenges
-const summonPuzzleInput = async () => {
-  const response = await fetch('https://adventofcode.com/2022/day/2/input');
-  const responseText = await response.text(); // wait for a response
-  return responseText; // return that response
-};
+// import fetch from 'fetch';
+// // Grab data from fetch input for daily challenges
+// const summonPuzzleInput = async () => {
+//   const response = await fetch('https://adventofcode.com/2022/day/2/input');
+//   const responseText = await response.text(); // wait for a response
+//   return responseText; // return that response
+// };
 
-const puzzleInput = await summonPuzzleInput();
+// const puzzleInput = await summonPuzzleInput();
 
-console.log(puzzleInput);
+// console.log(puzzleInput);
 
 
 // Capitalize the first letter of a word
-const str = 'hello';
+const str = 'hello there';
 
 function capitalizeFirstLetter (string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 console.log(capitalizeFirstLetter(str));
+
+// Capitalize all words of a string. 
+function capitalizeWords (string) {
+  return string.replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
+};
+
+console.log(capitalizeWords(str))
