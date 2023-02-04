8f967e564dcc46cc93b4437370ddc47fc9524bd0
Harry Fahringer III <viccem.dev@gmail.com>
2023-02-04 08:50:41 -0500

    Add recursive countdown function

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -26,3 +26,17 @@ function capitalizeWords (string) {
 };
 
 console.log(capitalizeWords(str))
+
+// Recursive Countdown
+
+function countdown(n){
+  if(n <= 0) {
+    return []
+  } else {
+  const countArray = countdown(n - 1)
+  countArray.unshift(n)
+  return countArray
+  }
+}
+
+console.log(countdown(10)); // log: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
