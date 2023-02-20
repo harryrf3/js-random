714724f25324add458e2abfcb86857ae0259c32d
Harry Fahringer III <viccem.dev@gmail.com>
2023-02-20 14:32:25 -0500

    ran prettier

--- snippets/bubbleSort.js
+++ snippets/bubbleSort.js
@@ -4,12 +4,12 @@ const bubbleSort = (array) => {
   const arr = Array.from(array);
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i; j++) {
-      if (arr[j] > arr[j+1]) {
-        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
   return arr;
 };
 
-console.log(bubbleSort(nums))
+console.log(bubbleSort(nums));
